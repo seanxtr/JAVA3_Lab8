0bfc5e0e1fdd3f2e02d8db12ea428919d8d94730
Student <Student@153.18.37.175>
2016-03-01 19:58:35 -0800

    Coding done, need to fix inden

--- JAVA3_Lab8/src/Foothill.java
+++ JAVA3_Lab8/src/Foothill.java
@@ -14,13 +14,14 @@ public class Foothill
       final int RL_MIN = 2;
       final int RL_MAX = 300;
       final int RL_INCREMENT = 2;
-      final int ARRAY_SIZE_MIN = 0;
-      final int ARRAY_SIZE_MAX = 1000000;
-      final int ARRAY_SIZE_INCREMENT = 100000;
+      final int ARRAY_SIZE_MIN = 20000;
+      final int ARRAY_SIZE_MAX = 1200000;
+      final int ARRAY_SIZE_INCREMENT = 50000;
       int k, randomInt, arraySize, limit;
       long startTime, stopTime; 
       Integer[] testArray;
       ArrayList<TestResult> results = new ArrayList<TestResult>();
+      TestResult minRt = null;
       
       // for formatting output neatly
       NumberFormat tidy = NumberFormat.getInstance(Locale.US);
@@ -35,7 +36,8 @@ public class Foothill
              arrayOfInts1[k] = randomInt;
          }  
          
-
+         minRt = new TestResult(0,0,Integer.MAX_VALUE);
+         
          for (limit = RL_MIN; limit <= RL_MAX; limit+= RL_INCREMENT) {
         	 FHsort.setRecursionLimit(limit);
         	 
@@ -49,12 +51,23 @@ public class Foothill
              System.out.printf("quickSort(%d,%d) Elapsed Time %s seconds%n",
                 limit, arraySize, tidy.format( (stopTime - startTime) / 1e9));
              
-             results.add(new TestResult(limit, arraySize, 
-                stopTime - startTime));
+             TestResult rt = new TestResult(limit, arraySize, stopTime - startTime);
+             if (rt.time < minRt.time)
+                minRt = rt;
+             results.add(rt);
          }
+         
+        minRt.isMin = true;
       }
       
-      //output results
+      System.out.printf("%nTable Summary%n-----------------------%n");
+      // output column headers
+      System.out.print(String.format("%-10s ", "RL/Size"));
+      for (limit = RL_MIN; limit <= RL_MAX; limit+= RL_INCREMENT)
+    	  System.out.print(String.format("%-8s", limit));
+      System.out.println();
+      
+      // output results
       Iterator<TestResult> itr = results.iterator();
       TestResult temp;
       int column = 0;
@@ -63,10 +76,9 @@ public class Foothill
     	  temp = (TestResult)itr.next();
     	  
     	  if (column == 0)
-    		  System.out.print(String.format("%-10s %-8s", temp.size,
-    			tidy.format(temp.time / 1e9)));
+    		  System.out.print(String.format("%-10s %-8s", temp.size, temp));
     	  else {
-    		  System.out.print(String.format("%-8s", tidy.format(temp.time / 1e9)));
+    		  System.out.print(String.format("%-8s", temp));
         	  if (column >= 149) {
         		  System.out.println();
         		  column = -1;
--- JAVA3_Lab8/src/TestResult.java
+++ JAVA3_Lab8/src/TestResult.java
@@ -1,11 +1,19 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class TestResult {
 	public int limit;
 	public int size;
 	public long time;
-   	   
+   	public boolean isMin;
+   	
 	public TestResult(int limit, int size, long time) {
 		this.limit = limit;
 		this.size = size;
 		this.time = time;
+	}
+	
+	public String toString() {
+		return String.format("%s%.4f", (isMin ? "*":""), time / 1e9);
 	}
 }
